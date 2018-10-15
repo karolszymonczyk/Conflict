b8baf260fb9f924a92692d67a8b4e5c8b51a9025
karolszymonczyk <karolszymonczyk@gmail.com>
2018-10-16 00:21:04 +0200

    How are you?

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello Simon!");
-        System.out.println("How are you?");
+        System.out.println("How are you Simon?");
     }
 }
